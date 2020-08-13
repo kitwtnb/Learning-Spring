b6260d29fee147d99c094a4d9fea0a91b02ad9d8
Keita Watanabe <watanabe.kit@gmail.com>
2020-08-13 12:36:01 +0900

    add nested

--- src/main/kotlin/com/github/kitwtnb/learning_spring/controller/HelloController.kt
+++ src/main/kotlin/com/github/kitwtnb/learning_spring/controller/HelloController.kt
@@ -11,4 +11,16 @@ class HelloController {
         // this is "resources/templates/hello.html"
         return "hello"
     }
+
+    @GetMapping("/nested_hello")
+    fun nestedHello1(): String {
+        // this is "resources/templates/nested/hello.html"
+        return "nested/nested_hello"
+    }
+
+    @GetMapping("/nested/hello")
+    fun nestedHello2(): String {
+        // this is "resources/templates/nested/hello.html"
+        return "nested/nested_hello"
+    }
 }
